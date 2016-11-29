94140921ae9c32a9f8169f25a768df56c40c7ca0
Heloá Meneses <heloa.meneses@gmail.com>
2016-11-28 23:05:28 -0200

    Handling employee creation and resetting the form

--- src/actions/types.js
+++ src/actions/types.js
@@ -6,3 +6,5 @@ export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 
 export const EMPLOYEE_FORM_UPDATE = 'employee_form_update';
+export const EMPLOYEE_CREATE = 'employee_create';
+
