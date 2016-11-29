e6783841c2bcb04e97e70673667128554d9b62dc
Heloá Meneses <heloa.meneses@gmail.com>
2016-11-29 20:37:07 -0200

    Importing lodash and, adjusting to default naming and adding ListItem

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,22 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, Text, View } from 'react-native';
-import { employeeFetch } from '../actions';
+import { ListView } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
-    this.props.employeeFetch();
+    this.props.employeesFetch();
+
     this.createDataSource(this.props);
   }
 
   componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+
     this.createDataSource(nextProps);
   }
 
@@ -18,23 +25,30 @@ class EmployeeList extends Component {
       rowHasChanged: (r1, r2) => r1 !== r2
     });
 
-    this.dataSource = ds.cloneWithRows(this.props.employees);
+    this.dataSource = ds.cloneWithRows(employees);
   }
-  
+
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     return (
-      <View>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
-export default connect(null, { employeeFetch })(EmployeeList);
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
