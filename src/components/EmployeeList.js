c1cdf288829b62ad4bd12bdcdbc66630117ee4df
Heloá Meneses <heloa.meneses@gmail.com>
2016-11-29 19:21:28 -0200

    Adding ListView component and logic to retrieve employees list

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView, Text, View } from 'react-native';
+import { employeeFetch } from '../actions';
+
+class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeeFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+      </View>
+    );
+  }
+}
+
+export default connect(null, { employeeFetch })(EmployeeList);
