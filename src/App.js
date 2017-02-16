d4c44ff5621ef339d4105ce2d1e379f3ea5ae2fc
Heloá Meneses <heloa.meneses@gmail.com>
2017-02-16 09:54:32 -0200

    Updating build.gradle with Android APK signing

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Router from './Router';
 class App extends Component {
   componentWillMount() {
     const config = {
-    apiKey: 'AIzaSyAvGc6RaAlL_9tsG3zo6X7pNuEkgpgTw8U',
-    authDomain: 'manager-80f31.firebaseapp.com',
-    databaseURL: 'https://manager-80f31.firebaseio.com',
-    storageBucket: 'manager-80f31.appspot.com',
-    messagingSenderId: '74522914544'
+    apiKey: '<firebase-info>',
+    authDomain: '<firebase-info>',
+    databaseURL: '<firebase-info>',
+    storageBucket: '<firebase-info>',
+    messagingSenderId: '<firebase-info>'
     };
     firebase.initializeApp(config);
   }
